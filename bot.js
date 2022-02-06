ce722911aaf34df1327e290be314088dba5ef73c
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-02-06 15:48:20 -0600

    testing more things with Steve, and talking to League of Legends API

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const dissBotIntents = new Intents();
 
-dissBotIntents.add('GUILDS', 'GUILD_MESSAGES')
+dissBotIntents.add('GUILDS', 'GUILD_MESSAGES');
 const client = new Client({ intents: dissBotIntents });
 
 client.on('ready', () => {
@@ -13,17 +13,20 @@ client.on('ready', () => {
 
 const roaster = require('./messageFunctions/roaster.js');
 const memer = require('./messageFunctions/memeMaker.js');
+const dataGetter = require('./statFunctions/dataGetter.js');
 // message is going to be the bread and butter, we need to break it down into different files / functions in order to make it easy to manage
 
 client.on('messageCreate', async (message) => {
   const splitMess = message.content.split(' ');
-  console.log(message);
-  console.log(splitMess);
+  // console.log(message);
+  // console.log(splitMess);
   if (splitMess[0] === '$diss') {
     const command = splitMess[1];
 
-    console.log(command);
-    if (!command) return;
+    // console.log(command);
+    if (!command) {
+      await message.reply('Please use one of the following commands: roast, meme, game');
+    }
 
     else if (command === 'roast') {
       await roaster(message);
@@ -32,7 +35,11 @@ client.on('messageCreate', async (message) => {
     else if (command === 'meme') {
       await memer(message);
     }
+
+    else if (command === 'game') {
+      await dataGetter(message);
+    }
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.DIS_TOKEN);
