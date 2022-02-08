80f40e769989c60ee6a233502977fdf51d23a4bf
Michael Metcalf <m.metcalf6453@gmail.com>
2022-02-07 17:56:05 -0800

    removed model and cleaned up routes

--- server/src/getPuuid.js
+++ server/src/getPuuid.js
@@ -7,19 +7,19 @@ let puuid;
 
 function getPuuid() {
   try {
-   const username = 'dariogoat';
-   const tagline = 'dgoat';
-   const url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${username}/${tagline}?api_key=${process.env.RIOT_API}`;
-   axios.get(url).then((value)=>{
+    const username = 'dariogoat';
+    const tagline = 'dgoat';
+    const url = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${username}/${tagline}?api_key=${process.env.RIOT_API}`;
+    axios.get(url).then((value)=>{
       puuid = value.data.puuid;
       console.log(puuid);
-   });
+    });
   // console.log(Data.data);
   }
   catch (error) {
     console.log(error);
   }
- }
+}
 getPuuid();
 
 
