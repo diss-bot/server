20afdd1a0466527080e60a51b5f0f8ce15feaeb4
Michael Metcalf <m.metcalf6453@gmail.com>
2022-02-08 17:10:36 -0800

    made it where once you register your in-game-name it will update the name and the puuid in the database

--- bot.js
+++ bot.js
@@ -64,6 +64,7 @@ client.on('messageCreate', async (message) => {
       author: msgAuthor,
       game: userInput[0],
       inGameName: userInput[1],
+      tagline: userInput[2],
     };
     client.commands.get('REGISTER').execute(message, updates);
   }
--- server/commands/updateUser.js
+++ server/commands/updateUser.js
@@ -1,49 +1,72 @@
 'use strict';
 
 const User = require('../models/userModel');
+const getLolPuuid = require('../util/getLolPuuid.js');
 
 module.exports = {
   name: 'REGISTER',
   description: 'Initializes desired game stat tracking',
   async execute(message, updates) {
     try {
-      // let userToUpdate = await User.find({ _id: `Captimus_Prime#9557` });
-      // let { games } = userToUpdate[0];
-      // // let { gameName } = updates;
-      // let gameName = 'TeamFightTactics';
-      // let { TeamFightTactics } = games;
 
-      let updates = {
-        TeamFightTactics: {
-          summonerName: '',
-          placement: 4,
-          wins: 1,
-        }
-      }
-      let updatedUser = await User.findByIdAndUpdate(`Captimus_Prime#9557`, {
-        $inc: {
-          "games.TeamFightTactics.win": updates.TeamFightTactics.wins,
-          "games.TeamFightTactics.placement": updates.TeamFightTactics.placement,
-        },
-        $set: {
-          "games.TeamFightTactics.summonerName": "I'm bricked right now"
-        }
-      });
+      let { author, game, inGameName, tagline } = updates;
+      let userPuuid = await getLolPuuid(inGameName);
 
-      console.log(updatedUser);
-    } catch (e) {
+      if(game.toLowerCase() === 'lol') {
+        let updatedUser = await User.findByIdAndUpdate(author, {
+          $set: {
+            "games.LeagueOfLegends.summonerName": inGameName, "puuid": userPuuid
+          },
+        });
+        console.log(updatedUser);
+      } else if(game.toLowerCase() === 'tft') {
+      
+        let updatedUser = await User.findByIdAndUpdate(author, {
+          $set: {
+            "games.TeamFightTactics.summonerName": inGameName, "puuid": userPuuid
+          },
+        });
+        console.log(updatedUser);
+      } else if(game.toLowerCase() === 'val') {
+      
+        let updatedUser = await User.findByIdAndUpdate(author, {
+          $set: {
+            "games.Valorant.gamerName": inGameName, "games.Valorant.tagline": tagline, "puuid": userPuuid
+          },
+        });
+        console.log(updatedUser);
+      } else if(game.toLowerCase() === 'apex') {
+      
+        let updatedUser = await User.findByIdAndUpdate(author, {
+          $set: {
+            "games.APEX.summonerName": inGameName, "puuid": userPuuid
+          },
+        });
+        console.log(updatedUser);
+        }
+
+
+
+
+    } catch(e) {
       message.channel.send(`You typed something in wrong. I don't know what but it was probably wrong.`);
       console.log(e);
     }
-  },
-};
-
-function helper(gameName) {
-  let values = [];
-  Object.entries(gameName).forEach(stat => {
-    values.push(stat);
-  })
-  console.log(values);
+  }
 }
+
+
+ // let updates = {
+      //   TeamFightTactics: {
+      //     summonerName: '',
+      //     placement: 4,
+      //     wins: 1,
+      //   },
+      // };
+
+// $inc: {
+        //   "games.TeamFightTactics.win": updates.TeamFightTactics.wins,
+        //   "games.TeamFightTactics.placement": updates.TeamFightTactics.placement,
+        // },
 
 //.schema.obj.games
