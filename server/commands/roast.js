f70a41e76b68f8ff2ea74912798efa0b4d6ea827
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-02-09 20:29:21 -0600

    embed is working and insulting people1

--- server/commands/roast.js
+++ server/commands/roast.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const compareUsers = require('../util/compareUsers.js');
+const compareStats = require('../util/compareStats.js');
 module.exports = {
   name: 'ROAST',
   description: 'Retrieve user data and roast the loser',
   async execute(message, game, users) {
     try {
-      let winner = await compareUsers(message, game, users);
-      message.channel.send(`${winner}`);
+      let embed = await compareStats(message, game, users);
+      message.channel.send({ embeds: [embed] });
     } catch (e) {
       console.log(e);
       message.channel.send('Ensure you input the correct name, you dunce');
